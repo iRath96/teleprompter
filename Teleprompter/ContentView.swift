dce6dcb419b56da60505ccf3adcec011e9af443b
Alexander Rath <alex@ist.besonders.cool>
2022-08-24 08:42:33 +0200

    Initial Commit

---
+++ Teleprompter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Teleprompter
+//
+//  Created by Alexander Rath on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teleprompter/TeleprompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleprompterApp.swift
+//  Teleprompter
+//
+//  Created by Alexander Rath on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TeleprompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
